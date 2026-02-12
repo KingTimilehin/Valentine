8c5fc593e6e8397c1c00a9f5061feb4abec6b859
KingTimilehin <108177657+KingTimilehin@users.noreply.github.com>
2026-02-12 10:34:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,20 +5,21 @@ const valLink = document.getElementById("valLink");
 const copyBtn = document.getElementById("copyBtn");
 
 generateBtn.addEventListener("click", () => {
-    const name = nameInput.ariaValueMax.trim();
-    if(name == ""){
-        alert("Oga enter her name");
-        return;
-    }
-     const link = `${window.location.origin}${window.location.pathname.replace("index.html","")}valentine.html?name=${encodeURIComponent(name)}`;
+  const name = nameInput.value.trim();
+
+  if (name === "") {
+    alert("Oga enter her name 😭❤️");
+    return;
+  }
+
+  const link = `${window.location.origin}${window.location.pathname.replace("index.html", "")}valentine.html?name=${encodeURIComponent(name)}`;
 
   valLink.value = link;
   linkBox.style.display = "block";
 });
 
 copyBtn.addEventListener("click", () => {
-  valLink.select();
-  document.execCommand("copy");
+  navigator.clipboard.writeText(valLink.value);
   copyBtn.innerText = "Copied! ✅";
 
   setTimeout(() => {
